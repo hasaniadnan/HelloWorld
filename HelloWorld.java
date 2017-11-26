e8cdf574907557aaae5f30722d6fa5e7a12aa6d0
adnanh <adnanhasanni@gmail.com>
2017-11-26 16:35:45 +0100

    added the addition variables

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,5 +5,8 @@ public class HelloWorld{
 			System.out.println("Hello World : " + i);
 		}
 		System.out.println("\n A Java Compilation file from Adnan");
+		int x = 10;
+		int y = 10;
+		int result = x + (y * 2);
 	}
 }
